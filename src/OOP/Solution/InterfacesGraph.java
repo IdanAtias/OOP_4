1247cda5da09290aff5ff53457f2e106565f79b9
IdanAtias <idan2k3@gmail.com>
2016-06-03 18:12:48 +0300

    Validate continue && DEBUG message for Lior

--- src/OOP/Solution/InterfacesGraph.java
+++ src/OOP/Solution/InterfacesGraph.java
@@ -8,186 +8,194 @@ import java.lang.reflect.Method;
 import OOP.Provided.OOPInherentAmbiguity;
 import OOP.Provided.OOPMultipleException;
 
+interface I5 {
+	void fa();
 
+	void fb();
 
-interface B
-{
-	   void fa();
-	   void fb();
-	   void fc();
-
-}
-
-interface C2 extends B
-{
+	void fc();
 
 }
 
-interface C1 extends B
+interface I4 extends I5
 {
 
 }
 
-interface D extends C1, C2
-{
-   void fa();
-   void fb();
+interface I3 extends I5 {
+
 }
 
-interface Base extends D
-{
+interface I2 extends I3, I4 {
+	void fa();
+	void fb();
+}
+
+interface I1 extends I2 {
 	void fc();
 }
 
 public class InterfacesGraph {
-	//graph of interfaces:
-	//vertices are interfaces
-	//edge u->v == u extends v
-	
-	/*TODO - CHANGE TO PRIVATE*/
-	public HashMap<Node, Class<?>[] > interfaces = new HashMap<Node, Class<?>[] >();
+	// graph of interfaces:
+	// vertices are interfaces
+	// edge u->v == u extends v
+
+	/* TODO - CHANGE TO PRIVATE */
+	public HashMap<Node, Class<?>[]> interfaces = new HashMap<Node, Class<?>[]>();
 	public Node base;
+
 	/*****************/
-	
-	InterfacesGraph(Class<?> baseInterClass) throws OOPMultipleException{
-		//TODO: throw compatible exception
-		base = new Node(baseInterClass,null);
+
+	InterfacesGraph(Class<?> baseInterClass) throws OOPMultipleException {
+		// TODO: throw compatible exception
+		base = new Node(baseInterClass, null);
 		buildGraph(base);
 	}
-	
+
 	private class Node {
 		public Class<?> inter;
 		public Node father;
+
 		public Node(Class<?> inter, Node father) {
 			this.inter = inter;
 			this.father = father;
 		}
-		
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			/*
+			 * interfaceClass doesn't have how to "hashCode" so we use its name
+			 * which is String that has "hashCode" implemented.
+			 */
+			//result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((inter == null) ? 0 : inter.getSimpleName().hashCode());
+			return result;
+		}
+
 		@Override
 		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof Node)) {
+				return false;
+			}
+			Node other = (Node) obj;
+			if (getOuterType() != (other.getOuterType())) {
+				return false;
+			}
+			if (inter == null) {
+				if (other.inter != null) {
+					return false;
+				}
+			} else if (!(inter.getSimpleName()).equals(other.inter.getSimpleName())) {
+				return false;
+			}
+			return true;
+		}
 
-//		@Override
-//		public boolean equals(Object obj) {
-//			if (this == obj) {
-//				return true;
-//			}
-//			if (obj == null) {
-//				return false;
-//			}
-//			if (!(obj instanceof Node)) {
-//				return false;
-//			}
-//			Node other = (Node) obj;
-//			if (!getOuterType().equals(other.getOuterType())) {
-//				return false;
-//			}
-//			if (inter == null) {
-//				if (other.inter != null) {
-//					return false;
-//				}
-//			} else if (inter != other.inter) {
-//				return false;
-//			}
-//			return true;
-//		}
-//		private InterfacesGraph getOuterType() {
-//			return InterfacesGraph.this;
-//		}
+		private InterfacesGraph getOuterType() {
+			return InterfacesGraph.this;
+		}
 	}
-	
+
 	private void buildGraph(Node interNode) throws OOPMultipleException {
-		//getting the interfaces baseInter extends:
+		// getting the interfaces baseInter extends:
 		Class<?>[] interfaces = interNode.inter.getInterfaces();
-		
-		//base cond: no interfaces extended then return
-		this.interfaces.put(interNode,interfaces);
-		if (interfaces.length == 0){	
+
+		// base cond: no interfaces extended then return
+		this.interfaces.put(interNode, interfaces);
+		if (interfaces.length == 0) {
 			return;
 		}
-		for (Class<?> iter: interfaces) {
+		for (Class<?> iter : interfaces) {
 			Node currNode = new Node(iter, interNode);
 			if (this.interfaces.containsKey(currNode)) {
-					checkForAmbiguity(currNode); //throws OOPinheritedAmbiguity if fails.
+				checkForAmbiguity(currNode); // throws OOPinheritedAmbiguity if
+												// fails.
 			}
 			buildGraph(currNode);
 		}
 	}
-	
-	private void checkForAmbiguity(Node interNode) throws OOPMultipleException{
+
+	private void checkForAmbiguity(Node interNode) throws OOPMultipleException {
 		LinkedList<Node> path = getPathToBase(interNode.father);
 		Node existsNode = null;
-		for (Node curr: interfaces.keySet()){
-			if (curr.equals(interNode)){
+		for (Node curr : interfaces.keySet()) {
+			if (curr.equals(interNode)) {
 				existsNode = curr;
 				break;
 			}
 		}
 		Node intersection = getIntersection(existsNode, path);
-		LinkedList<Method> methods= new LinkedList<Method>();
-		for (Method m: interNode.inter.getMethods()) {
+		LinkedList<Method> methods = new LinkedList<Method>();
+		for (Method m : interNode.inter.getMethods()) {
 			methods.add(m);
 		}
-		checkForAmbiguityAux(intersection, methods,interNode);
+		checkForAmbiguityAux(intersection, methods, interNode);
 	}
 
-	private void checkForAmbiguityAux(Node intersection, LinkedList<Method> methods, Node faulty) throws OOPMultipleException {
+	private void checkForAmbiguityAux(Node intersection, LinkedList<Method> methods, Node faulty)
+			throws OOPMultipleException {
 		LinkedList<Node> path = getPathToBase(intersection);
-		for (Node n: path) {
+		for (Node n : path) {
 			Method[] nMethods = n.inter.getMethods();
 			for (Method m : nMethods) {
 				methods.remove(m);
 			}
 		}
-		if (methods.isEmpty()) return;
+		if (methods.isEmpty())
+			return;
 		throw new OOPInherentAmbiguity(base.inter, faulty.inter, methods.getFirst());
 	}
 
 	private Node getIntersection(Node existsNode, LinkedList<Node> path) {
-		LinkedList<Node> existNodePath=getPathToBase(existsNode.father);
-		for (Node n: existNodePath) {
-			if (path.contains(n)) return n;
+		LinkedList<Node> existNodePath = getPathToBase(existsNode.father);
+		for (Node n : existNodePath) {
+			if (path.contains(n))
+				return n;
 		}
-		return base;//shouldn't get to here.
+		return base;// shouldn't get to here.
 	}
 
 	private LinkedList<Node> getPathToBase(Node interNode) {
 		LinkedList<Node> path = new LinkedList<Node>();
 		while (interNode != null) {
 			path.add(interNode);
-			interNode=interNode.father;
+			interNode = interNode.father;
 		}
 		return path;
 	}
-	
-	
-	public void printGraph()
-	{
-		for (Node i: interfaces.keySet()) {
-			System.out.println("The current interface: "+i.inter);
-			if (i.father != null){
-				System.out.println("The current interface father : "+i.father.inter);
+
+	public void printGraph() {
+		for (Node i : interfaces.keySet()) {
+			System.out.println("The current interface: " + i.inter.getSimpleName());
+			if (i.father != null) {
+				System.out.println("The current interface father : " + i.father.inter.getSimpleName());
 			}
 			System.out.print("and he extends: ");
-			for (Class<?> v: interfaces.get(i)) {
-				System.out.print(" "+v);
+			for (Class<?> v : interfaces.get(i)) {
+				System.out.print(" " + v.getSimpleName());
 			}
 			System.out.println();
 			System.out.println();
 		}
-}
-	
-	
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	}
+
+	public static void main(String[] args) throws java.lang.Exception {
 		try {
-		InterfacesGraph g = new InterfacesGraph(Base.class);
-		g.printGraph();
-		} catch (OOPInherentAmbiguity e){
+			InterfacesGraph g = new InterfacesGraph(I1.class);
+			g.printGraph();
+		} catch (OOPInherentAmbiguity e) {
 			System.out.println(e.getMessage());
 			return;
 		}
 		System.out.println("SUCCESS");
 	}
-	
-}
 
+}
